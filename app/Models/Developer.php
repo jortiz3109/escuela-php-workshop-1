2dfc05deff6334c3e87920b34d6e9cdff3e24cbe
John Edisson Ortiz <j.ortiz3109@gmail.com>
2021-10-04 14:08:20 -0500

    Added filters for developer model

--- app/Models/Developer.php
+++ app/Models/Developer.php
@@ -2,32 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
+use App\Filters\Concerns\HasFilters;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Developer extends Model
 {
     use HasFactory;
-
-    public function scopeByName(Builder $query, string $name): Builder
-    {
-        return $query->where('name', 'like', "%{$name}%");
-    }
-
-    public function scopeByEmail(Builder $query, string $email): Builder
-    {
-        return $query->where('email', 'like', "%{$email}");
-    }
-
-    public function scopeFilter(Builder $query, array $filters = []): Builder
-    {
-        foreach ($filters as $filter => $value) {
-            $scopeName = Str::camel("scopeBy{$filter}");
-            $this->{$scopeName}($query, $value);
-        }
-
-        return $query;
-    }
+    use HasFilters;
 }
