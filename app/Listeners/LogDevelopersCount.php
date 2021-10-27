9add5f371f91044a9688028d87004e79e6c21c70
John Edisson Ortiz <j.ortiz3109@gmail.com>
2021-10-26 22:17:27 -0500

    fix: Fix code smell from sonarcloud

--- app/Listeners/LogDevelopersCount.php
+++ app/Listeners/LogDevelopersCount.php
@@ -2,13 +2,12 @@
 
 namespace App\Listeners;
 
-use App\Events\DeveloperCreated;
 use App\Models\Developer;
 use Illuminate\Support\Facades\Log;
 
 class LogDevelopersCount
 {
-    public function handle(DeveloperCreated $event): void
+    public function handle(): void
     {
         Log::info('Developers count: ' . Developer::count());
     }
